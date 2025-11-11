8f5354cb5a4fb76dd3830d0fe8e8320fa90681bc
anitawiwiw <anaviri789@gmail.com>
2025-11-11 08:14:17 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+public function run(): void
+{
+    $this->call([
+        AulasSeeder::class,
+        DocentesSeeder::class,
+        MateriasSeeder::class,
+        ReservasSeeder::class,
+    ]);
+}
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
     }
 }
